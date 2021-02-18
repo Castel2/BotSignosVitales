6939723b8614afe5a083a7052d79690504074c12
Castel2 <yordancas@hotmail.com>
2021-02-17 22:37:28 -0500

    consultar signos #18

--- GestorComandos.py
+++ GestorComandos.py
@@ -127,7 +127,7 @@ def on_delete_signos(message):
 
     print(respuesta)
    
-@bot.message_handler(regexp=r"^(consultar signos|cs)$")
+@bot.message_handler(regexp=r"^(consultar signos|cs) ([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) ([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
 def on_get_signos(message):
     bot.send_chat_action(message.chat.id, 'typing')
     text = message.chat.first_name
--- GestorConsultas.py
+++ GestorConsultas.py
@@ -5,11 +5,8 @@ from models.Usuario import Usuario
 from models.Medicion import Medicion
 
 def get_signos (user_id):
-    print(user_id)
     signos = db.session.query(Medicion).filter(Medicion.id_user_tel == user_id).all()
     db.session.commit()
-    print(signos)
     if not signos:
-        print("ENTROOOOOOOOOOOOOO")
         return None
     return signos
